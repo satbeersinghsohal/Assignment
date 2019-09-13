9a21b4e0003b1290a00e57dff77e87826bc258fc
satbeer <satbeersinghsohal@gmail.com>
2019-09-13 20:11:22 +0530

    add commets to the code

--- index.js
+++ index.js
@@ -2,24 +2,30 @@ var express  = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var db = mongoose.connect('mongodb://localhost/test',{useNewUrlParser:true});
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 var app = express();
 
+
+// importing Models from model folder
 var Client = require('./model/client');
 var User = require('./model/User');
+
 var userdetails = null;
+
+//#################### adding middleware ###########################
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-
-
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/success', (req, res) => res.send("Welcome "+req.query.username+"!!"));
-app.get('/error', (req, res) => res.send("error logging in"));
+//###################### middleware end#################
+
+//##################### authentication or passport middleware######################
+
 
 passport.serializeUser(function(user, cb) {
   cb(null, user.id);
@@ -31,32 +37,6 @@ passport.deserializeUser(function(id, cb) {
   });
 });
 
-
-app.post("/form",function(req,res){
-	// var data = {
-	// 	name:req.body.name ,
-	// 	email:req.body.email ,
-	// 	course:req.body.course ,
-	// 	tel:req.body.tel
-	// }
-	console.log(req.body.username,
-    req.body.email,
-    req.body.tel,
-    req.body.course)
-	var user = new Client();
-    // user.userid   = req.body.userid;
-    user.username = req.body.username;
-    user.email    = req.body.email;
-    user.tel      = req.body.tel;
-    user.course    = req.body.course;
-    user.save(function(err, saveduser){
-        if(err){
-            res.status(404).send({error:"could not saved"})
-        }else {
-            res.send({"msg":saveduser});
-        }
-    })
-})
 
 
 passport.use(new LocalStrategy(
@@ -89,82 +69,113 @@ passport.use(new LocalStrategy(
   }
 ));
 
-app.post('/',
-  passport.authenticate('local', { failureRedirect: '/error' }),
-  function(req, res) {
-
-  	console.log("success");
-    res.redirect('/dashboard');
-  });
 
 
+//################ All routes ###############################
+
+	app.get('/success', (req, res) => res.send("Welcome "+req.query.username+"!!"));
+	app.get('/error', (req, res) => res.send("error logging in"));
+
+	app.post("/form",function(req,res){ // client form 
+
+		console.log(req.body.username,
+	    req.body.email,
+	    req.body.tel,
+	    req.body.course)
+		var user = new Client();
+	    // user.userid   = req.body.userid;
+	    user.username = req.body.username;
+	    user.email    = req.body.email;
+	    user.tel      = req.body.tel;
+	    user.course    = req.body.course;
+	    user.save(function(err, saveduser){
+	        if(err){
+	            res.status(404).send({error:"could not saved"})
+	        }else {
+	            res.send({"msg":saveduser});
+	        }
+	    })
+	})
+
+	//################# employee login route ###############################
+
+	app.post('/',
+	  passport.authenticate('local', { failureRedirect: '/error' }),
+	  function(req, res) {
+
+	  	console.log("success");
+	    res.redirect('/dashboard');
+	  });
 
 
-app.post('/newuser',function(req, res) {
-  	console.log("success")
-  	console.log("username",req.body.username);
-  	var user = new User();
-  	user.username = req.body.username;
-  	user.password = req.body.password;
-    user.save(function(err, saveduser){
-        if(err){
-            res.status(404).send({error:"could not saved"})
-        }else {
-            res.send({"msg":saveduser});
-        }
-    })
-    res.redirect('/dashboard');
-  });
+
+	//##################### sign up new employee route ############################
+	app.post('/newuser',function(req, res) {
+	  	console.log("success")
+	  	console.log("username",req.body.username);
+	  	var user = new User();
+	  	user.username = req.body.username;
+	  	user.password = req.body.password;
+	    user.save(function(err, saveduser){
+	        if(err){
+	            res.status(404).send({error:"could not saved"})
+	        }else {
+	            res.send({"msg":saveduser});
+	        }
+	    })
+	    res.redirect('/dashboard'); // redirecting to dashboard
+	  });
 
 
 
 
+    //###################### Fetch Leads (all claimed or unclaimed leads) ######################### 
+	app.get("/dashboard",function(req,res) { 
 
-app.get("/dashboard",function(req,res) {
+		console.log("dashboard");
+		// console.log(Object.keys( req));
+		if(!userdetails.username){
+			return res.send({"msg":"your not logged in"})
+		}
 
-	console.log("dashboard");
-	// console.log(Object.keys( req));
-	if(!userdetails.username){
-		return res.send({"msg":"your not logged in"})
-	}
+		console.log(userdetails.username);
 
-	console.log(userdetails.username);
+		if(req.query.page == "myleads"){
+			Client.find({"claim":true,"claimedby":userdetails.username},function(err,data){
+				res.send(data);
+			})
+		}else
+		{
+			Client.find({"claim":false},function(err,data){
+				res.send(data);
+			})
+		}
 
-	if(req.query.page == "myleads"){
-		Client.find({"claim":true,"claimedby":userdetails.username},function(err,data){
-			res.send(data);
-		})
-	}else
-	{
-		Client.find({"claim":false},function(err,data){
-			res.send(data);
-		})
-	}
+	})
 
-})
+	//###################### Lead claimed by employee Route #####################################
+	app.post("/dashboard",function(req,res) {
 
-app.post("/dashboard",function(req,res) {
+		if(!userdetails.username){
+			res.send({"msg":"your not logged in"})
+		}
 
-	if(!userdetails.username){
-		res.send({"msg":"your not logged in"})
-	}
-
-	// console.log(userdetails.username);
-	if(req.body.claim  && req.body.clientemail != null){
-		Client.update({"email":req.body.clientemail},{"claim":true,"claimedby":userdetails.username},function(err,data){
-			if(err){
-				res.send(err);
-			}else{
-				
-			res.send(data);
-			}
-		})
-	}else{
+		// console.log(userdetails.username);
+		if(req.body.claim  && req.body.clientemail != null){
+			Client.update({"email":req.body.clientemail},{"claim":true,"claimedby":userdetails.username},function(err,data){
+				if(err){
+					res.send(err);
+				}else{
+					
+				res.send(data);
+				}
+			})
+		}else{
 
 
-	res.send({"error":"claim not given"})
-	}
-})
+		res.send({"error":"claim not given"})
+		}
+	})
 
 
 
